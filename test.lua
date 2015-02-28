f1e197d00c5db7369a3e60a73c0eb3211251bc3a
fanyh <28048856@qq.com>
2015-02-28 15:32:49 +0800

    fix test code

--- test.lua
+++ test.lua
@@ -1,25 +1,23 @@
 local RedEnv = require "red_envelope"
-local all = {}
+local run_times = 100
 
-for i = 1, 100000 do
-    local total = 100--math.random(100,10000)
-    local times = 10--math.random(2,100)
-    local red_env = RedEnv.new(total,times, nil , nil ,0)
+local random_total_min = 100
+local random_total_max = 10000
+
+local random_times_min = 2
+local random_times_max = 10
+
+for i = 1, run_times do
+    local total = math.random(random_total_min, random_total_max)
+    local times = math.random(random_times_min,random_times_max)
+    local red_env = RedEnv.new(total,times)
     local money = red_env:random()
-    local s= "第"..i.."次：总数"..total.."，次数："..times.."："
+    local s= "times:"..i.."total:"..total.."random times:"..times..":"
     local tmp = 0
     for i, v in ipairs(money) do
         s = s .. v ..","
         tmp = tmp + v
-        if all[i] == nil then
-            all[i] = 0
-        end
-        all[i] = all[i] + v
     end
+    print(s)
 end
 
-local a = ""
-for k , v in pairs(all) do
-    a = a .. k ..":".. v .. ","
-end
-print(a)
